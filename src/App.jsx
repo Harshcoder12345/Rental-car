1b1d58ce5ef260aef07f57548fee190015b537c9
Harshcoder12345 <harshjain611998@gmail.com>
2025-03-12 00:52:56 +0530

    second

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import Navbar from './component/Navbar'
+import Header from './component/Header'
 
 const App = () => {
   return (
     <div className='bg-red-800' >App
     
     <Navbar/>
+    <Header/>
     </div>
 
   )
