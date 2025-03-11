fcece03b97ce3a665993022598cf94de7c860eff
Harshcoder12345 <harshjain611998@gmail.com>
2025-03-11 22:11:06 +0530

    firstN

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,13 @@
 import React from 'react'
+import Navbar from './component/Navbar'
 
 const App = () => {
   return (
-    <div className='bg-red-800' >App</div>
+    <div className='bg-red-800' >App
+    
+    <Navbar/>
+    </div>
+
   )
 }
 
