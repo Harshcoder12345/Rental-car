921d3b07226d5d84cf8d1d1e9ac577cb634bffaa
Shriyansh Ozarkar <sozarkar7@gmail.com>
2025-03-12 16:02:03 +0530

    add footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 import React from 'react'
 
 const App = () => {
@@ -7,3 +8,22 @@ const App = () => {
 }
 
 export default App
+=======
+import React from "react";
+import Navbar from "./component/Navbar";
+import Header from "./component/Header";
+import Footer from "./component/Footer";
+
+const App = () => {
+  return (
+    <div className="bg-red-800">
+      App
+      <Navbar />
+      <Header />
+      <Footer />
+    </div>
+  );
+};
+
+export default App;
+>>>>>>> Stashed changes
